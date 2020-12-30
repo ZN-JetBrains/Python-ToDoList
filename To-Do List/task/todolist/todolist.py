0a3ddfbe7838cd1a311d1bc3aadbae85e11f8fa2
Zaid Neurothrone <neurothrone@outlook.com>
2020-12-30 03:35:15 +0100

    Complete Stage 3/4: Deadlines are scary

--- To-Do List/task/todolist/todolist.py
+++ To-Do List/task/todolist/todolist.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
+from datetime import timedelta
 
 Base = declarative_base()
 
@@ -26,8 +27,44 @@ session = Session()
 
 def print_menu():
     print("1) Today's tasks")
-    print("2) Add task")
+    print("2) Week's tasks")
+    print("3) All tasks")
+    print("4) Add task")
     print("0) Exit")
+
+
+def print_today_tasks():
+    date = datetime.today()
+    print(date.strftime("\nToday %#d %b:"))
+    print_tasks(date)
+
+
+def print_week_tasks(date):
+    print(date.strftime("\n%A %#d %b:"))
+    print_tasks(date)
+
+
+def print_tasks(date):
+    tasks = session.query(Table).filter(Table.deadline == date.date()).order_by(Table.deadline).all()
+
+    if len(tasks) == 0:
+        print("Nothing to do!")
+    else:
+        for index, row in enumerate(tasks):
+            print(f"{index + 1}. {row.task}")
+
+
+def print_all_tasks():
+    rows = session.query(Table).order_by(Table.deadline).all()
+    
+    if len(rows) == 0:
+        print("Nothing to do!\n")
+    else:
+        print("\nAll tasks:")
+        for index, row in enumerate(rows):
+            date = datetime.strptime(str(row.deadline), "%Y-%m-%d").date()
+            date = date.strftime("%#d %b")
+            print(f"{index + 1}. {row.task}. {date}")
 
 
 def run():
@@ -38,22 +75,25 @@ def run():
         if user_input == 0:
             print("\nBye!")
             break
-        elif user_input == 1:  # Display all tasks
-            print("\nToday:")
-            # if not empty
-            rows = session.query(Table).all()
-            if len(rows) == 0:
-                print("Nothing to do!\n")
-            else:
-                for index, row in enumerate(rows):
-                    print(f"{index + 1}. {row.task}")
-                print()
-
-        elif user_input == 2:  # Add a row
+        elif user_input == 1:  # Display all tasks for today
+            print_today_tasks()
+            print()
+        elif user_input == 2:  # Display all tasks for this Week
+            today = datetime.today()
+            for day in range(7):
+                current_date = today + timedelta(days=day)
+                print_week_tasks(current_date)
+            print()
+        elif user_input == 3:  # Display all tasks in database
+            print_all_tasks()
+            print()
+        elif user_input == 4:  # Add a row
             print("\nEnter task")
             user_task = input()
+            print("Enter deadline")  # format: YYYY-MM-DD
+            deadline_date = input()
             new_row = Table(task=user_task,
-                            deadline=datetime.strptime("01-24-2020", "%m-%d-%Y").date().today())
+                            deadline=datetime.strptime(deadline_date, "%Y-%m-%d").date())
             session.add(new_row)
             session.commit()
             print("The task has been added!\n")
